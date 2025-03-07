77b3667f796706b0c9d7c9e2949be798c442c98f
Danielr263-dev <danielri263@yahoo.com>
2025-03-07 14:28:55 -0500

    Added left,right,up,down animations (midly dangerous)
    Added left,right,up, down, animations but controller wasn't added so they're just there I think. Careful with this one. Animations aren't being applied either at all or not properly.

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 10f; // Speed of movement
+    public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 movement;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Get Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Get movement input
-        movement.x = Input.GetAxisRaw("Horizontal"); // Left (-1) / Right (+1)
-        movement.y = Input.GetAxisRaw("Vertical");   // Down (-1) / Up (+1)
+        // Get player input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize movement to avoid diagonal speed boost
+        movement = movement.normalized;
+
+        // Set Animator parameters
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetBool("isMoving", movement.magnitude > 0);
     }
 
     void FixedUpdate()
     {
-        // Apply movement using Rigidbody2D
-        rb.velocity = movement.normalized * moveSpeed;
+        // Move the player
+        rb.velocity = movement * moveSpeed;
     }
 }
